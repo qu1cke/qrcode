06be89e28ce9b22bb4d96ad68edc2126421e1cd5
qu1cke <rene.springborn@me.com>
2021-08-05 18:01:19 +0200

    added docker

---
+++ qrcode/routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+const qr = require("qrcode");
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/:code', function(req, res, next) {
+  console.log(req.params.code)
+
+  qr.toFile("qrcode.png",req.params.code,(err) => {
+    if (err) res.send("Error occured")
+    res.download("qrcode.png")
+  })   
+});
+
+module.exports = router;
